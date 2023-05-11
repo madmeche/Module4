f38c16c4d36dc55c7abc4001f81e98b85f0db942
madmeche <madeleine.meche@gmail.com>
2023-05-11 14:44:49 -0500

    slide exercises and labs

---
+++ exercises.js
@@ -0,0 +1,38 @@
+/*Exercise 1 :
+Modify the function so that you can pass the content
+for the card dynamically.
+
+Exercise 2 :
+Modify the code so that your cards are automatically
+generated based on data from an array. This way you
+will create as many cards as you need to display all the
+data in the array.
+
+Exercise 3 - the artist’s portfolio:
+Generate multiple templates and populate the page
+dynamically. Create profile card and generate cards
+representing an artist's portfolio.
+
+For exercise 2 use the following array :
+
+const data = [{name: 'bob', age: 23}, {name: 'alice', age: 39}]
+
+For exercise 3 use the following:
+
+const artist={
+name:"Van Gogh",
+portfolio:[
+{title:"portrait",
+
+url:"https://collectionapi.metmuseum.org/api/collecti
+on/v1/iiif/436532/1671316/main-image"},
+
+{title:"sky",
+
+url:"https://mymodernmet.com/wp/wp-
+content/uploads/2020/11/White-house-night-van-goh-
+worldwide-2.jpg"},
+
+]
+}
+*/
